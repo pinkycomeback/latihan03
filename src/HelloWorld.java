3476e6749ef500e75428172e6d00a51d92bfdf7a
pinkycomeback <danufirmansyah.java@gmail.com>
2018-12-04 10:59:31 +0700

    Hello World commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,10 @@
 public class HelloWorld{
 	public static void main (String []args){
 		System.out.println("Hello World again");
-                System.out.println("Hi!");
+                System.out.println("Hi!!");
 	}
+
+
 }
+
+
